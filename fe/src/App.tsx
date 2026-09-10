cfff526169d3bcbdcfadf459f2de89acbcb8c81f
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-09-10 16:11:15 +0700

    feat: update components for Homepage(Summary)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,13 @@
 import AscendDashboard from './dashboard'
 import SideNav from './components/sideNav'
 import Progress from './components/progress'
+import CreateBar from './components/createBar'
+import CreatePage from './routes/CreatePage'
 
 function App() {
   return (
     <div data-theme="dim" >
-      <Progress/>
+      <CreatePage/>
     </div>
   )
 }
